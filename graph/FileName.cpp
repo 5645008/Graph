18857df997e5494834c4d73473b5f2f41da74cf3
BuildTools <unconfigured@null.spigotmc.org>
2023-11-13 21:32:53 +0900

    Dijkstra-최단경로(과제)-연결리스트

--- graph/FileName.cpp
+++ graph/FileName.cpp
@@ -2,97 +2,123 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define TRUE 1
-#define FALSE 0
-#define MAX_VERTICES	100	
-#define INF	1000000	           // 무한대 (연결이 없는 경우) 
+#define MAX_VERTICES 100
+#define INF INT_MAX
 
-typedef struct GraphType
-{
-	int n;	                  // 정점의 개수
-	int weight[MAX_VERTICES][MAX_VERTICES];
+typedef struct ListNode {
+    int vertex;
+    int weight;
+    struct ListNode* link;
+} ListNode;
+
+typedef struct GraphType {
+    int n;
+    ListNode* adj_list[MAX_VERTICES];
 } GraphType;
 
-int distance[MAX_VERTICES];   // 시작정점으로부터의 최단경로 거리 
-int found[MAX_VERTICES];      // 방문한 정점 표시 
+int distance[MAX_VERTICES];
+int found[MAX_VERTICES];
 int MST[MAX_VERTICES] = { 0 };
 
-int choose(int distance[], int n, int found[])
-{
-	int i, min, minpos;
-	min = INT_MAX;
-	minpos = -1;
-	for (i = 0; i < n; i++)
-		if (distance[i] < min && !found[i]) {
-			min = distance[i];
-			minpos = i;
-		}
-	return minpos;
+int get_min_vertex(int n) {
+    int v, i;
+    for (i = 0; i < n; i++) {
+        if (!found[i]) {
+            v = i;
+            break;
+        }
+    }
+    for (i = 0; i < n; i++) {
+        if (!found[i] && (distance[i] < distance[v])) {
+            v = i;
+        }
+    }
+    return v;
 }
+
+void add_edge(GraphType* g, int start, int end, int weight) {
+    ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+    node->vertex = end;
+    node->weight = weight;
+    node->link = g->adj_list[start];
+    g->adj_list[start] = node;
+}
+
 //출력 부분
 void print_status(GraphType* g) {
-	printf("Dijkstra Algorithm\n");
-	printf("Distance:");
-	for (int i = 0; i < g->n; i++) {
-		if (distance[i] == INF) {
-			printf("* ");
-		}
-		else {
-			printf("%d ", distance[i]);
-		}
-	}
-	printf("\n");
-	printf("Found:   ");
-	for (int i = 0; i < g->n; i++) {
-		printf("%d ", found[i]);
-	}
-	printf("\n\n");
+    printf("Dijkstra Algorithm\n");
+    printf("Distance:");
+    for (int i = 0; i < g->n; i++) {
+        if (distance[i] == INF) {
+            printf("* ");
+        }
+        else {
+            printf("%d ", distance[i]);
+        }
+    }
+    printf("\n");
+    printf("Found:   ");
+    for (int i = 0; i < g->n; i++) {
+        printf("%d ", found[i]);
+    }
+    printf("\n\n");
 }
 
 
-// Dijkstra의 최단 경로 프로그램
-void shortest_path(GraphType* g, int start)
-{
-	int i, u, w;
-	for (i = 0; i < g->n; i++)   /* 초기화 */
-	{
-		distance[i] = g->weight[start][i];
-		found[i] = FALSE;
-	}
-	found[start] = TRUE;    /* 시작 정점 방문 표시 */
-	distance[start] = 0; 
-	for (i = 0; i < g->n - 1; i++)
-	{
-		print_status(g);
-		u = choose(distance, g->n, found);
-		found[u] = TRUE;
-		MST[i+1] = u;
-		for (w = 0; w < g->n; w++)
-			if (!found[w])
-				if (distance[u] + g->weight[u][w] < distance[w])
-					distance[w] = distance[u] + g->weight[u][w];
-	}
+void dijkstra(GraphType* g, int s) {
+    int i, u, v;
+    ListNode* p;
+    for (u = 0; u < g->n; u++) {
+        distance[u] = INF;
+    }
+    distance[s] = 0;
+    for (i = 0; i < g->n; i++) {
+        print_status(g);
+        u = get_min_vertex(g->n);
+        found[u] = 1;
+        MST[i + 1] = u;
+        if (distance[u] == INF) {
+            return;
+        }
+        p = g->adj_list[u];
+        while (p != NULL) {
+            v = p->vertex;
+            if (!found[v] && p->weight < distance[v]) {
+                distance[v] = p->weight;
+            }
+            p = p->link;
+        }
+    }
 }
 
-int main(void)
-{
-	GraphType g = { 10,
-		{{0,3,INF,INF,INF,11,12,INF,INF,INF},
-		{3,0,5,4,1,7,8,INF,INF,INF},
-		{INF,5,0,2,INF,INF,6,5,INF,INF},
-		{INF,4,2,0,13,INF,INF,14,INF,16},
-		{INF,1,INF,13,0,9,INF,INF,18,17},
-		{11,7,INF,INF,9,0,INF,INF,INF,INF},
-		{12,8,6,INF,INF,INF,0,INF,13,INF},
-		{INF,INF,5,14,INF,INF,13,0,INF,15},
-		{INF,INF,INF,INF,18,INF,INF,INF,0,10},
-		{INF,INF,INF,16,17,INF,INF,15,10,0}}
-	};
-	shortest_path(&g, 0);
+int main(void) {
+    GraphType g = { 10, {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL} };
+    add_edge(&g, 0, 1, 3);
+    add_edge(&g, 0, 5, 11);
+    add_edge(&g, 0, 6, 12);
+    add_edge(&g, 1, 2, 5);
+    add_edge(&g, 1, 3, 4);
+    add_edge(&g, 1, 4, 1);
+    add_edge(&g, 1, 5, 7);
+    add_edge(&g, 1, 6, 8);
+    add_edge(&g, 2, 3, 2);
+    add_edge(&g, 2, 6, 6);
+    add_edge(&g, 2, 7, 5);
+    add_edge(&g, 3, 4, 13);
+    add_edge(&g, 3, 7, 14);
+    add_edge(&g, 3, 9, 16);
+    add_edge(&g, 4, 5, 9);
+    add_edge(&g, 4, 8, 18);
+    add_edge(&g, 4, 9, 17);
+    add_edge(&g, 6, 7, 13);
+    add_edge(&g, 7, 9, 15);
+    add_edge(&g, 8, 9, 10);
+    dijkstra(&g, 0);
 
-	//MST 출력부분
-	for (int i = 0; i < g.n; i++) {
-		printf("%d ", MST[i] + 1);
-	}
-	return 0;
+    //MST 출력부분
+    for (int i = 0; i < g.n; i++) {
+        printf("%d ", MST[i] + 1);
+    }
+
+    return 0;
 }
