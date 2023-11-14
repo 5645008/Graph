2a1845a385cec25e6f9a8a2fce63a8b288f47256
5645008 <5645008@stu.kmu.ac.kr>
2023-11-14 10:48:53 +0900

    Floyd-최단경로(실습)

--- graph/FileName.cpp
+++ graph/FileName.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 #define TRUE 1
 #define FALSE 0
 #define MAX_VERTICES	100	
-#define INF	1000000	           // 무한대 (연결이 없는 경우) 
+#define INF	1000000	           /* 무한대 (연결이 없는 경우) */
 
 typedef struct GraphType
 {
@@ -13,66 +12,53 @@ typedef struct GraphType
 	int weight[MAX_VERTICES][MAX_VERTICES];
 } GraphType;
 
-int distance[MAX_VERTICES];   // 시작정점으로부터의 최단경로 거리 
-int found[MAX_VERTICES];      // 방문한 정점 표시 
-int MST[MAX_VERTICES] = { 0 };
+int A[MAX_VERTICES][MAX_VERTICES];
+int P[MAX_VERTICES][MAX_VERTICES];
+int S[MAX_VERTICES];
 
-int choose(int distance[], int n, int found[])
+void floyd(GraphType* g)
 {
-	int i, min, minpos;
-	min = INT_MAX;
-	minpos = -1;
-	for (i = 0; i < n; i++)
-		if (distance[i] < min && !found[i]) {
-			min = distance[i];
-			minpos = i;
+
+	int i, j, k;
+	for (i = 0; i < g->n; i++)
+		for (j = 0; j < g->n; j++) {
+			A[i][j] = g->weight[i][j];
+			if (g->weight[i][j] != 0 && g->weight[i][j] != INF) {
+				P[i][j] = j;
+			}
+			else {
+				P[i][j] = -1;
+			}
 		}
-	return minpos;
-}
-//출력 부분
-void print_status(GraphType* g) {
-	printf("Dijkstra Algorithm\n");
-	printf("Distance:");
-	for (int i = 0; i < g->n; i++) {
-		if (distance[i] == INF) {
-			printf("* ");
-		}
-		else {
-			printf("%d ", distance[i]);
-		}
+
+	for (k = 0; k < g->n; k++)
+	{
+		for (i = 0; i < g->n; i++)
+			for (j = 0; j < g->n; j++)
+				if (A[i][k] + A[k][j] < A[i][j]) {
+					A[i][j] = A[i][k] + A[k][j];
+					P[i][j] = k;
+				}
 	}
-	printf("\n");
-	printf("Found:   ");
-	for (int i = 0; i < g->n; i++) {
-		printf("%d ", found[i]);
-	}
-	printf("\n\n");
 }
 
+void shortway(int start, int end) {
+	int s = P[end][start];
+	int i = 0;
+	S[i] = end;
+	i++;
+	while (s != -1) {
+		S[i] = s;
+		i++;
+		s = P[s][start];
+	}
+	for (int j = i-1; j > 0; j--) {
+		printf("%d->", S[j]+1);
+	}
+	printf("%d\n", S[0] + 1);
 
-// Dijkstra의 최단 경로 프로그램
-void shortest_path(GraphType* g, int start)
-{
-	int i, u, w;
-	for (i = 0; i < g->n; i++)   /* 초기화 */
-	{
-		distance[i] = g->weight[start][i];
-		found[i] = FALSE;
-	}
-	found[start] = TRUE;    /* 시작 정점 방문 표시 */
-	distance[start] = 0; 
-	for (i = 0; i < g->n - 1; i++)
-	{
-		print_status(g);
-		u = choose(distance, g->n, found);
-		found[u] = TRUE;
-		MST[i+1] = u;
-		for (w = 0; w < g->n; w++)
-			if (!found[w])
-				if (distance[u] + g->weight[u][w] < distance[w])
-					distance[w] = distance[u] + g->weight[u][w];
-	}
 }
+
 
 int main(void)
 {
@@ -88,11 +74,26 @@ int main(void)
 		{INF,INF,INF,INF,18,INF,INF,INF,0,10},
 		{INF,INF,INF,16,17,INF,INF,15,10,0}}
 	};
-	shortest_path(&g, 0);
-
-	//MST 출력부분
-	for (int i = 0; i < g.n; i++) {
-		printf("%d ", MST[i] + 1);
+	floyd(&g);
+	
+	int start, end = 0;
+	printf("Floyd - Warshall Algorithm\n");
+	while (1) {
+		printf("Start Node: ");
+		scanf_s("%d", &start);
+		printf("End Node: ");
+		scanf_s("%d", &end);
+		printf("Shortest Distance: %d\n", A[start-1][end-1]);
+		printf("최단 경로: ");
+		shortway(start-1, end-1);
+		printf("\n");
 	}
+	
+	
+
+
+	
+
+
 	return 0;
 }
